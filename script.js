f4a550f741a4b97625c00bfa6ac351e53ad199f3
TerroR75 <kamilsulimierski@gmail.com>
2022-05-13 12:29:13 +0200

    Feature - working pixel generation

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
 const gridPanel = document.querySelector('.right-panel');
-// let tileNumber = 16;
-
-// createTiles();
+const rangeInput = document.querySelector('.rangeInput')
 
 
-// function createTiles() {
-//     for (let i = 0; i < tileNumber * 14; i++) {
-//         let tile = document.createElement('div');
-//         let tileSize = 500 / tileNumber;
-//         tile.style.width = `${tileSize}px`;
-//         tile.style.height = `${tileSize}px`;
-//         tile.style.transform = `translate(${tileSize})`;
-//         tile.classList.add('gridtile');
-//         gridPanel.appendChild(tile);
-//     }
+// EVENTS
+rangeInput.addEventListener('change', () => {
+    deleteTiles();
+    createTiles(rangeInput.value);
+});
 
-// }
+
+createTiles();
+
+function createTiles(tileNumber = 16) {
+    for (let i = 0; i < tileNumber ** 2; i++) {
+        let tile = document.createElement('div');
+        let tileSize = 500 / tileNumber;
+        tile.style.width = `${tileSize}px`;
+        tile.style.height = `${tileSize}px`;
+        tile.style.transform = `translate(${tileSize})`;
+        tile.classList.add('gridtile');
+        gridPanel.appendChild(tile);
+    }
+
+    // gridPanel.style.gridTemplateColumns = `repeat(${tileNumber}, auto)`;
+    // gridPanel.style.gridTemplateRows = `repeat(${tileNumber}, auto)`;
+
+}
+
+function deleteTiles() {
+    let tiles = document.querySelectorAll('.gridtile');
+    for (let tile of tiles) {
+        gridPanel.removeChild(tile);
+    }
+}
