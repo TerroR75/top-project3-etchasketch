88c20d1c89e21a929e60ef92b111823b67c43ada
TerroR75 <kamilsulimierski@gmail.com>
2022-05-13 02:12:13 +0200

    Changed layout + some initial styles

---
+++ script.js
@@ -0,0 +1,18 @@
+const gridPanel = document.querySelector('.right-panel');
+// let tileNumber = 16;
+
+// createTiles();
+
+
+// function createTiles() {
+//     for (let i = 0; i < tileNumber * 14; i++) {
+//         let tile = document.createElement('div');
+//         let tileSize = 500 / tileNumber;
+//         tile.style.width = `${tileSize}px`;
+//         tile.style.height = `${tileSize}px`;
+//         tile.style.transform = `translate(${tileSize})`;
+//         tile.classList.add('gridtile');
+//         gridPanel.appendChild(tile);
+//     }
+
+// }
