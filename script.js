4695239318f9c6df3f6da871c7d2894eecf5ecb1
TerroR75 <kamilsulimierski@gmail.com>
2022-05-13 13:19:03 +0200

    feat - tile coloring and clearing

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
 const gridPanel = document.querySelector('.right-panel');
-const rangeInput = document.querySelector('.rangeInput')
+
+// range input
+const rangeInput = document.querySelector('.rangeInput');
+const rangeInputText = document.querySelector('.rangeInputContainer>p');
+
+// clear input
+const clearBtn = document.querySelector('.clearBtn');
+
+// INITAL
+createTiles();
+let isMouseDown = false;
+document.body.onmousedown = () => isMouseDown = true;
+document.body.onmouseup = () => isMouseDown = false;
 
 
 // EVENTS
 rangeInput.addEventListener('change', () => {
     deleteTiles();
     createTiles(rangeInput.value);
+    updateText(rangeInputText, `${rangeInput.value} x ${rangeInput.value}`)
 });
 
+clearBtn.addEventListener('click', clearTiles);
 
-createTiles();
 
-function createTiles(tileNumber = 16) {
+
+function createTiles(tileNumber = 2) {
+    gridPanel.style.gridTemplateColumns = `repeat(${tileNumber}, auto)`;
+    gridPanel.style.gridTemplateRows = `repeat(${tileNumber}, auto)`;
     for (let i = 0; i < tileNumber ** 2; i++) {
         let tile = document.createElement('div');
         let tileSize = 500 / tileNumber;
@@ -19,17 +35,30 @@ function createTiles(tileNumber = 16) {
         tile.style.height = `${tileSize}px`;
         tile.style.transform = `translate(${tileSize})`;
         tile.classList.add('gridtile');
+        tile.addEventListener('mouseover', changeTileColor);
+        tile.addEventListener('mousedown', changeTileColor);
         gridPanel.appendChild(tile);
     }
 
-    // gridPanel.style.gridTemplateColumns = `repeat(${tileNumber}, auto)`;
-    // gridPanel.style.gridTemplateRows = `repeat(${tileNumber}, auto)`;
 
 }
 
 function deleteTiles() {
+    gridPanel.innerHTML = '';
+}
+
+function updateText(element, text) {
+    element.textContent = text;
+}
+
+function changeTileColor(e) {
+    if (e.type === 'mouseover' && !isMouseDown) return;
+    this.style.backgroundColor = 'black';
+}
+
+function clearTiles() {
     let tiles = document.querySelectorAll('.gridtile');
     for (let tile of tiles) {
-        gridPanel.removeChild(tile);
+        tile.style.backgroundColor = 'inherit';
     }
 }
